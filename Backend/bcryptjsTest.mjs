2c339084d0dc6fad2b8bbf5644640ec9173e0d76
Kapil Reddy Sanikommu <kapilreddy55555@gmail.com>
2025-10-26 20:55:20 -0700

    solved layout issue of Dashboard

--- Backend/bcryptjsTest.mjs
+++ Backend/bcryptjsTest.mjs
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 
 const hash = '$2b$10$ZJcdMOzjI.VwGOYJCwDT0.aHZuY.gQF.3oDP6t/eXYsgZAILFKNdC'
-const password = "123456";
+const password = "Kapil@123";
 
 const match = await bcrypt.compare(password, hash);
 console.log("Does it match?", match);
--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -6,7 +6,7 @@ export const ENV = {
   ORIGIN: process.env.ORIGIN || 'http://localhost:5173',
   DB_HOST: process.env.DB_HOST || '127.0.0.1',
   DB_USER: process.env.DB_USER || 'root',
-  DB_PASS: process.env.DB_PASS || '1234',
+  DB_PASS: process.env.DB_PASS || 'Kapil@123',
   DB_NAME: process.env.DB_NAME || 'airbnb',
   SESSION_SECRET: process.env.SESSION_SECRET || 'devsecret',
   SESSION_NAME: process.env.SESSION_NAME || 'sid'
