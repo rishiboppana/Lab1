caaeb271bf6f2059b76ba9e727d9d8bb6bfb8112
Gitpracticerishi <rishiboppana24@gmail.com>
2025-11-14 16:49:06 -0800

    Propert Model in MongoDb

---
+++ Backend/services/property_service/model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const propertySchema = mongoose.Schema({
+    id : Number , 
+    owner_id : Number , 
+    title : String , 
+    location : String,
+    description : String , 
+    price_per_night : Number ,
+    bedrooms : Number , 
+    amenities : [String],
+    images : [String] , 
+    number_of_guests : Number
+} , {timestamps : true})
+
+const propertyModel = mongoose.model('Properties' , propertySchema)
+
+module.exports  = propertyModel
